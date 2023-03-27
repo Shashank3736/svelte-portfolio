eac6ce7f63be4c2831115deec13fb0734e766ea3
Shashank3736 <shreyashr267@gmail.com>
2023-03-27 22:43:10 +0530

    Down Button

---
+++ src/components/Down.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let link = "#next";
+</script>
+
+<a href={link} class="absolute bottom-0 left-1/2 translate-x-[-50%] py-4">
+    <div class="left-0 right-0 mx-auto text-white animate-bounce p-4 bg-white/20 rounded-full">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
+        </svg>         
+    </div>
+</a>
