843e41947202cc5cb0dbf7f0980dd634a2f81de3
Shashank3736 <shreyashr267@gmail.com>
2023-03-27 22:44:22 +0530

    Added my name

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,6 +1,6 @@
 <div class="flex flex-row justify-between m-4 py-4 px-10 border-4 border-white/40 rounded-2xl backdrop-blur-xl bg-white/20 shadow-2xl">
     <div class="flex flex-row items-center space-x-2">
-        <h1 class="text-2xl font-bold">Navbar</h1>
+        <h1 class="text-2xl font-bold">Shreyash Raj</h1>
     </div>
     <div class="space-x-4 my-auto">
         <a href="/">Home</a>
