30b5e052b5d896bee4fe223ca88b0e87b9524ddb
Shashank3736 <shreyashr267@gmail.com>
2023-03-27 16:47:28 +0530

    added tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+    h1 {
+        @apply text-4xl font-bold text-red-400;
+    }
+</style>
