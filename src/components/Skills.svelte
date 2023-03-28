ad23191f505cf53257515b76f8c66289abb1a89a
Shashank3736 <shreyashr267@gmail.com>
2023-03-28 22:27:00 +0530

    improved small devices look

--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -29,17 +29,17 @@
         'SQLite',
         'PostgreSQL',
         'NoSQL',
-        'C Programming',
+        'C Language',
         'MS Office',
     ]
 </script>
 <div id="skills" class="relative flex justify-center h-[100vh] p-4">
-    <div class="container overflow-auto m-10 mb-20 p-10 border-4 border-white/40 rounded-xl bg-white/20">
+    <div class="container overflow-auto sm:m-10 mb-20 p-10 border-4 border-white/40 rounded-xl bg-white/20">
         <h1 class="text-4xl font-semibold text-center">Skills</h1>
         <hr class="my-4 border-2">
         <div class="flex flex-wrap justify-center">
             {#each skills as skill}
-                <div class="m-2 p-2 border-2 transition ease-in-out duration-500 delay-150 hover:bg-white hover:shadow-lg hover:text-indigo-600 hover:scale-110 border-white/40 rounded-xl">
+                <div class="m-2 px-4 py-2 border-2 min-w-[100px] text-center transition ease-in-out duration-500 delay-150 hover:bg-white hover:shadow-lg hover:text-indigo-600 hover:scale-110 border-white/40 rounded-xl">
                     {skill}
                 </div>
             {/each}
