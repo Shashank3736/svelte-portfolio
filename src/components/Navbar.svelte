7b19fc2f2e051c905d0db0332e4e632dc9a9e457
Shashank3736 <shreyashr267@gmail.com>
2023-03-28 11:17:04 +0530

    added go to top button

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,4 +1,28 @@
-<div class="flex flex-col sm:flex-row justify-between m-4 py-4 px-10 border-4 border-white/40 rounded-2xl backdrop-blur-xl bg-white/20 shadow-2xl">
+<script>
+    import { onMount } from 'svelte';
+    const goTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+    onMount(() => {
+        window.addEventListener('scroll', () => {
+            const top_button = document.getElementById('go-to-top');
+            const navbar = document.getElementById('navbar');
+            if (top_button === null) return;
+            if (navbar === null) return;
+            console.log(navbar.getBoundingClientRect().bottom);
+            if (navbar.getBoundingClientRect().bottom < 0) {
+                top_button.classList.remove('hidden');
+            } else {
+                top_button.classList.add('hidden');
+            }
+        });
+    })
+</script>
+
+<div id='navbar' class="flex flex-col sm:flex-row justify-between m-4 py-4 px-10 border-4 border-white/40 rounded-2xl backdrop-blur-xl bg-white/20 shadow-2xl">
     <div class="flex flex-row items-center space-x-2">
         <h1 class="text-2xl font-bold">Shreyash Raj</h1>
     </div>
@@ -10,6 +34,11 @@
         <a href="#contact">Contact</a>
     </div>
 </div>
+<button id='go-to-top' on:click={goTop} class="fixed right-4 bottom-4 z-50 hidden animate-bounce text-indigo-500 bg-white/20 p-4 rounded-full hover:cursor-pointer">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+    </svg>      
+</button>
 
 <style>
     * {
