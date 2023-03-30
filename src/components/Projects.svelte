6962acc6a315c87c24db56705f4e631c53af3203
Shashank3736 <shreyashr267@gmail.com>
2023-03-30 23:33:35 +0530

    removed keframe as it supports global one from app.css

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -64,16 +64,4 @@
         background-size: 500% 500%;
         animation: gradient 5s ease infinite;
    }
-
-   @keyframes gradient {
-    0% {
-        background-position: 0% 50%;
-    }
-    50% {
-        background-position: 100% 50%;
-    }
-    100% {
-        background-position: 0% 50%;
-    }
-}
 </style>
