fedfd2df0e51fb096c8e6d8810ce223e6a630f18
Shashank3736 <shreyashr267@gmail.com>
2023-03-31 13:35:56 +0530

    update projects to support graphql data

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -3,33 +3,15 @@
     import caloriemeter from "../assets/caloriemeter.png";
     import calapi from "../assets/cal-api.png";
     type Project = {
-        name: string,
-        description: string,
-        image: string,
+        title: string,
+        description: {html: string},
+        thumbnail: { url: string }
         link: string,
-        github: string
+        github: string,
+        id: string
     }
-    const projects: Project[] = [
-        {
-            name: "Caloriemeter",
-            description: "A website made using NextJS, TailwindCSS, and Material UI. It is a calorie counter website that allows users to track their calories and save it. It uses a Calorimeter API to get the data.",
-            image: caloriemeter,
-            link: "https://cm.shreyashraj.com/",
-            github: "https://github.com/Shashank3736/caloriemeter"
-        }, {
-            name: "Caloriemeter API",
-            description: "A REST API made using Python and Django. It is a REST API that allows users to get, post, update, and delete data from the database. Allow them to read, create and update user, create tokens and add and delete foods.",
-            image: calapi,
-            link: "https://api.cm.shreyashraj.com/api/",
-            github: "https://github.com/Shashank3736/caloriemeter"
-        }, {
-            name: 'captcha-canvas',
-            description: 'A captcha generator by using skia-canvas. It creates captchas which OCR can\'t read. It is made using TypeScript and Skia Canvas.',
-            image: 'https://raw.githubusercontent.com/Shashank3736/captcha-canvas/master/examples/example.png',
-            link: 'https://www.npmjs.com/package/captcha-canvas',
-            github: 'https://github.com/Shashank3736/captcha-canvas'
-        }
-    ]
+    
+    export let projects: Project[];
 </script>
 
 <div id='project' class="relative sm:p-5 container mx-auto min-h-full">
@@ -39,10 +21,10 @@
         <div class={"flex flex-row space-x-4 overflow-x-auto relative " + (projects.length > 3 ? "": "xl:justify-center ") + (projects.length > 2 ? "" : "lg:justify-center")}>
             {#each projects as project}
                 <div class="gradient-bg flex flex-col grow scale-90 transition ease-in-out delay-150 duration-300 hover:scale-100 p-5 space-y-4 rounded-lg border-2 min-w-[300px] max-w-[18rem] shrink-0 shadow-lg hover:shadow-2xl">
-                    <h1 class="text-2xl md:text-3xl font-bold text-center">{project.name}</h1>
+                    <h1 class="text-2xl md:text-3xl font-bold text-center">{project.title}</h1>
                     <hr class="my-4 border-2">
-                    <p class="text-center grow">{project.description}</p>
-                    <img src={project.image} alt={project.name} class="w-full object-cover rounded-lg">
+                    <p class="text-center grow">{@html project.description.html}</p>
+                    <img src={project.thumbnail.url} alt={project.title} class="w-full object-cover rounded-lg">
                     <div class='flex flex-row space-x-8 items-center justify-center'>
                         <a class="bg-gray-700/80 transition ease-in-out delay-150 duration-300 hover:scale-110 hover:bg-gray-700 px-4 py-2 rounded" href={project.github}>GitHub</a>
                         <a class="bg-indigo-700/80 transition ease-in-out delay-150 duration-300 hover:scale-110 hover:bg-indigo-700 px-4 py-2 rounded" href={project.link}>Live</a>
