4393bb35c64e93fad8673d8032db31c4b540a734
Shashank3736 <shreyashr267@gmail.com>
2023-03-31 13:36:20 +0530

    updated skills to support graphql data

--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -1,37 +1,45 @@
-<script>
+<script lang='ts'>
 	import Down from "./Down.svelte";
-    const skills = [
-        'CSS',
-        'Git',
-        'SQL',
-        'HTML',
-        'Rust',
-        'NoSQL',
-        'Nginx',
-        'Linux',
-        'SQLite',
-        'GitHub',
-        'Python',
-        'NextJS',
-        'Svelte',
-        'Django',
-        'Node.js',
-        'ReactJS',
-        'MongoDB',
-        'VS Code',
-        'Git Bash',
-        'REST API',
-        'MS Office',
-        'Bootstrap',
-        'C Language',
-        'PostgreSQL',
-        'JavaScript',
-        'Algorithms',
-        'TypeScript',
-        'TailwindCSS',
-        'LibreOffice',
-        'Data Structures',
-    ]
+    // const skills = [
+    //     'CSS',
+    //     'Git',
+    //     'SQL',
+    //     'HTML',
+    //     'Rust',
+    //     'NoSQL',
+    //     'Nginx',
+    //     'Linux',
+    //     'SQLite',
+    //     'GitHub',
+    //     'Python',
+    //     'NextJS',
+    //     'Svelte',
+    //     'Django',
+    //     'Node.js',
+    //     'ReactJS',
+    //     'MongoDB',
+    //     'VS Code',
+    //     'Git Bash',
+    //     'REST API',
+    //     'MS Office',
+    //     'Bootstrap',
+    //     'C Language',
+    //     'PostgreSQL',
+    //     'JavaScript',
+    //     'Algorithms',
+    //     'TypeScript',
+    //     'TailwindCSS',
+    //     'LibreOffice',
+    //     'Data Structures',
+    // ]
+
+    type Skills = {
+        id: String,
+        name: String,
+        level: "Beginner" | "Intermediate" | "Advanced"
+    }
+
+    export let skills: Skills[];
 </script>
 <div id="skills" class="relative flex justify-center h-[100vh] py-5 md:px-20">
     <div class="container overflow-auto sm:m-20 sm:p-20 p-5 border-4 border-white/40 rounded-xl bg-white/20">
@@ -40,7 +48,7 @@
         <div class="flex flex-wrap justify-center">
             {#each skills as skill}
                 <div class="m-2 px-4 py-2 border-2 min-w-[100px] text-center transition ease-in-out duration-500 delay-150 hover:bg-white hover:shadow-lg hover:text-indigo-600 hover:scale-110 border-white/40 rounded-xl">
-                    {skill}
+                    {skill.name}
                 </div>
             {/each}
         </div>
