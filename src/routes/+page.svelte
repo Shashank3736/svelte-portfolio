6da5901368ae0d54e3e0a22b7bad4ac0e774e993
Shashank3736 <shreyashr267@gmail.com>
2023-03-31 13:36:40 +0530

    updated to support new skills and projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
-    import { About, Navbar, Skills, Projects, ProgressBar, Footer } from "../components"
+    import { About, Navbar, Skills, Projects, ProgressBar, Footer } from "../components";
+    export let data;
 </script>
 
 <ProgressBar />
 <Navbar />
 <About />
-<Skills />
-<Projects />
+<Skills skills={data.skills} />
+<Projects projects={data.projects} />
 <Footer />
