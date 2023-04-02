b34feafb4aef3c27a64de205d8c12e7b6d414ade
Shashank3736 <shreyashr267@gmail.com>
2023-04-02 12:49:39 +0530

    improved css

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -11,17 +11,17 @@
     export let data: Blog;
 </script>
 
-<div class="container my-5 mx-auto p-5">
-    <div class="bg-white/20 border-2 border-white/40 rounded-xl w-full p-5">
-        <img src={data.thumbnail.url} alt={data.title} class="w-full h-96 object-cover rounded-xl">
+<div class="my-5 sm:p-5">
+    <div>
         <div class="flex flex-col items-center justify-center p-5">
-            <div class="relative prose p-4">
+            <div class="relative prose lg:prose-xl bg-white/60 b border-4 border-white/40 rounded-2xl w-full px-2 sm:px-10 py-5">
                 <h1 class="text-4xl font-semibold text-center">{data.title}</h1>
-                <hr class="my-4 border-2">
+                <img src={data.thumbnail.url} alt={data.title} class="w-full max-h-96 object-cover rounded-xl">
+                <hr class="border-2">
                 <article class="text-lg">
                     {@html data.content.html}
                 </article>
-                <p class="absolute bottom-2 right-2 text-gray-400">
+                <p class="absolute text-base bottom-2 right-2 text-gray-600">
                     - By Shreyash Raj on {moment(data.publishedAt).format("MMMM Do YYYY")}
                 </p>
             </div>
