710d34b77e9d99e6e1c34b39a2f337a2b0cfd2c3
Shashank3736 <shreyashr267@gmail.com>
2023-04-02 21:05:44 +0530

    added a div for gradient background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,18 @@
 	import { Footer, Navbar, ProgressBar } from "../components";
 </script>
 
-<ProgressBar />
-<Navbar />
-<slot />
-<Footer />
+<div id='main' class="gradient_bg min-h-screen">
+    <ProgressBar />
+    <Navbar />
+    <slot />
+    <Footer />
+</div>
+
+<style>
+    .gradient_bg {
+        width: 100%;
+        background: linear-gradient(45deg, #1d4ed8, #db2777, #3730a3, #ea580c);
+        background-size: 800% 800%;
+        animation: gradient 15s ease infinite;
+    }
+</style>
