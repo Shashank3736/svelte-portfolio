790a117468ab5b29860bebc57e778ef319d798a1
Shashank3736 <shreyashr267@gmail.com>
2023-04-02 16:36:31 +0530

    improved css

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -17,22 +17,39 @@
         </div>
     </div>
 {:else}
-    <h1 class="text-4xl font-semibold text-center">Blogs</h1>
+    <h1 class="text-4xl font-semibold text-center mb-4">Blogs</h1>
     <div class="container mx-auto min-h-screen">
         {#each data.blogs as blog}
-            <div class="flex flex-row items-center justify-center p-5">
-                <div class="flex flex-col-reverse relative md:flex-row items-center p-8 border-4 w-full border-white/40 rounded-xl bg-white/20">
-                    <img src={blog.thumbnail.url} alt={blog.title} class="w-96 object-cover rounded-xl">
-                    <div class="flex flex-col items-center justify-center p-5">
-                        <div class="relative prose-invert px-4 mb-2">
-                            <h1 class="text-4xl font-semibold text-center mb-2">{blog.title}</h1>
-                            {@html blog.description.html}
-                        </div>
-                        <a href="/blog/{blog.slug}/" class="self-end mt-4 text-blue-300 font-bold text-lg underline hover:text-indigo-400">Read More >></a>
+            <div class="flex flex-col-reverse relative md:flex-row p-4 border-4 w-full h-min border-white/40 rounded-xl bg-white/20">
+                <a href="/blog/{blog.slug}/" class="md:absolute bottom-2 right-2 mt-4 text-blue-300 font-bold text-lg rounded-md hover:bg-white/20 px-4 py-2">Read More >></a>
+                <a href="/blog/{blog.slug}/" class="max-w-[354px] shrink-0 relative rounded-xl show-div">
+                    <div class="visible-div absolute top-0 left-0 rounded-xl items-center justify-center backdrop-blur-md w-full h-full">
+                        <p class="text-2xl font-bold">
+                            {moment(blog.publishedAt).format("MMMM Do YYYY")}
+                        </p>
+                    </div>
+                    <img src={blog.thumbnail.url} alt={blog.title} class="object-cover rounded-xl">
+                </a>
+                <div class="bg-gray-200/60 ml-4 w-1 hidden md:flex" />
+                <div class="flex flex-col items-center justify-center w-full h-full p-4">
+                    <div class="relative prose-invert h-full">
+                        <h1 class="text-4xl font-semibold text-center mb-2">{blog.title}</h1>
+                        {@html blog.description.html}
                     </div>
                 </div>
             </div>
-            <hr class="m-4 border-2 border-white/20">
+            {#if blog.slug !== data.blogs[data.blogs.length - 1].slug}
+                <hr class="m-4 border-2 border-white/20">
+            {/if}
         {/each}
     </div>
 {/if}
+
+<style>
+    .show-div div{
+        display: none;
+    }
+    .show-div:hover .visible-div {
+        display: flex;
+    }
+</style>
