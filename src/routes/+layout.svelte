58ad344449e07e6cbea3841385adfd91d7f30c14
Shashank3736 <shreyashr267@gmail.com>
2023-04-21 13:37:27 +0530

    added web analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '../app.css';
 	import { Footer, Navbar, ProgressBar } from '../components';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div id="main" class="gradient_bg min-h-screen">
