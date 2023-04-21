c3a3f3d4d3c4d23856918b06c193137fa277a86c
Shashank3736 <shreyashr267@gmail.com>
2023-04-21 13:51:32 +0530

    check 1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script>
 	import '../app.css';
 	import { Footer, Navbar, ProgressBar } from '../components';
-	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
-	inject({ mode: dev ? 'development' : 'production' });
+	inject();
 </script>
 
 <div id="main" class="gradient_bg min-h-screen">
