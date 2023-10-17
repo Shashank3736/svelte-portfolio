1d190cd001af17bc9cb7e6477457222205162c63
Shashank3736 <shreyashr267@gmail.com>
2023-10-17 15:29:47 +0530

    updated background color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	inject();
 </script>
 
-<div id="main" class="min-h-screen bg-slate-300 dark:bg-slate-800 dark:text-white">
+<div id="main" class="min-h-screen from-slate-100 to-slate-300 dark:from-slate-500 dark:to-slate-900 bg-gradient-to-tr da dark:text-white">
 	<ProgressBar />
 	<Navbar />
 	<slot />
